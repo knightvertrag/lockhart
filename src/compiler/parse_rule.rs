c64283cec9a79c119296dcfa41ccf64db27b6860
knightvertrag <amiapu1@gmail.com>
2023-05-05 19:07:39 +0530

    added parsing rule table

---
+++ src/compiler/parse_rule.rs
@@ -0,0 +1,66 @@
+use crate::token::{TokenType::{self, *}};
+
+use super::{precedence::Precedence, precedence::Precedence::*, Parser};
+
+macro_rules! rule {
+    ($a: ident, $tok: ident, $prefix: expr, $infix: expr, $precedence: expr) => {
+        $a[$tok as usize] = ParseRule {
+            prefix: $prefix,
+            infix: $infix,
+            precedence: $precedence,
+        }
+    };
+}
+type ParseFn<'a> = fn(&'a mut Parser<'a>) -> ();
+#[derive(Clone, Copy)]
+pub struct ParseRule<'a> {
+    pub prefix: Option<ParseFn<'a>>,
+    pub infix: Option<ParseFn<'a>>,
+    pub precedence: Precedence,
+}
+
+impl<'a> ParseRule<'a> {
+    pub fn get_rule(token_type: TokenType) -> &'static ParseRule<'a> {
+        &RULES[token_type as usize]
+    }
+}
+
+pub static RULES: [ParseRule; 30] = {
+    let mut a = [ParseRule {
+        prefix: None,
+        infix: None,
+        precedence: PrecNone,
+    }; 30];
+    rule!(a, IDENT, None, None, PrecNone);
+    rule!(a, NUM, Some(Parser::number), None, PrecNone);
+    rule!(a, LITERAL, None, None, PrecNone);
+    rule!(a, LET, None, None, PrecNone);
+    rule!(a, FUNCTION, None, None, PrecNone);
+    rule!(a, IF, None, None, PrecNone);
+    rule!(a, ELSE, None, None, PrecNone);
+    rule!(a, RETURN, None, None, PrecNone);
+    rule!(a, TRUE, None, None, PrecNone);
+    rule!(a, FALSE, None, None, PrecNone);
+    rule!(a, ASSIGN, None, None, PrecNone);
+    rule!(a, NOT, None, None, PrecNone);
+    rule!(a, GT, None, None, PrecNone);
+    rule!(a, LT, None, None, PrecNone);
+    rule!(a, GEQ, None, None, PrecNone);
+    rule!(a, LEQ, None, None, PrecNone);
+    rule!(a, EQ, None, None, PrecNone);
+    rule!(a, NEQ, None, None, PrecNone);
+    rule!(a, PLUS, None, Some(Parser::binary), PrecTerm);
+    rule!(a, MINUS, Some(Parser::unary), Some(Parser::binary), PrecTerm);
+    rule!(a, MUL, None, Some(Parser::binary), PrecFactor);
+    rule!(a, DIV, None, Some(Parser::binary), PrecFactor);
+    rule!(a, COMMA, None, None, PrecNone);
+    rule!(a, SEMICOLON, None, None, PrecNone);
+    rule!(a, LBRACE, Some(Parser::grouping), None, PrecNone);
+    rule!(a, RBRACE, Some(Parser::grouping), None, PrecNone);
+    rule!(a, LPAREN, Some(Parser::grouping), None, PrecNone);
+    rule!(a, RPAREN, Some(Parser::grouping), None, PrecNone);
+    rule!(a, ILLEGAL, None, None, PrecNone);
+    rule!(a, EOF, None, None, PrecNone);
+
+    a
+};
