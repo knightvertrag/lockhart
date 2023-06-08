dda08135549c2e5918373c52fc27b5fcfd68e1e5
knightvertrag <amiapu1@gmail.com>
2023-06-09 05:26:48 +0530

    switched back to repl

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,20 @@ mod repl;
 mod token;
 mod value;
 mod vm;
+mod object;
 use bytecode::Opcode;
 use chunk::{Chunk, Lineno};
 use vm::Vm;
 fn main() -> io::Result<()> {
-    // let args: Vec<String> = env::args().collect();
-    // if args.len() == 1 {
-    //     println!("Hello, Coompiler!");
-    //     repl::start();
-    // } else {
-    //     let src_filename = &args[1];
-    // }
-    let s = " 4 != 4";
-    let mut interpreter = Vm::init_vm();
-    interpreter.interpret(s.to_string());
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("===============Lockhart initiated===============");
+        repl::start();
+    } else {
+        let src_filename = &args[1];
+    }
+    // let s = "4 == nil";
+    // let mut interpreter = Vm::init_vm();
+    // interpreter.interpret(s.to_string());
     Ok(())
 }
